7408a655b88450e3db54e7d36cde1b04411f2564
Nishank Siddharth <45737944+ethereal999@users.noreply.github.com>
2019-08-26 14:38:27 +0530

    Update Lab4.cpp

--- Lab4.cpp
+++ Lab4.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stack>
 #include <bits/stdc++.h>
+#include <map>
 using namespace std;
 
 
@@ -36,9 +37,14 @@ struct node* newNode(string str){
 
 }
 
-struct node* createtree(string s){
+struct node* createtree(string s, map<string,int> *m1){
         stack<char> st1;
         stack<struct node*> st2;
+
+        int checkcon=0;
+
+
+
         for(int i=0;i<s.length();i++){
 
           if(s[i]>='0' && s[i]<='9'){
@@ -51,12 +57,15 @@ struct node* createtree(string s){
              }
              string t=to_string(temp);
            struct node* t1= newNode(t);
-
+        if(checkcon==0){
+           checkcon=-1;
+        }
            st2.push(t1);
             i=j-1;
             }
 
           else if(s[i]=='('){
+            checkcon=0;
            st1.push(s[i]);
           }
           else if(s[i]=='+' || s[i]=='-' || s[i]=='*' || s[i]=='^' || s[i]=='/'){
@@ -72,6 +81,9 @@ struct node* createtree(string s){
               }
 
            else if(checkpre(s[i])>checkpre(st1.top())){
+                if(checkcon==0){
+                  checkcon=1;
+                }
                  st1.push(s[i]);
 
             }
@@ -119,6 +131,11 @@ struct node* createtree(string s){
 
                    t1->left =un;
                 }
+                else if(checkcon==1){
+                                   struct node* un=newNode("0");
+
+                   t1->left =un;
+                }
                 else{
                 t1->left=st2.top();
                 st2.pop();
@@ -132,7 +149,47 @@ struct node* createtree(string s){
 
 
           }
+          else{
+                     int p=i;
+                     string tmp;
+               for(p=i;p<s.length();p++){
+                   if(!(s[p]>='a' && s[p]<='z' || s[p]>='A' && s[p]<='Z')){
+                           break;
+                   }
+                   else{
+                    tmp.push_back(s[p]);
+
+                   }
+
+               }
+
+
+
+
+
+                    auto itr= (*m1).find(tmp);
+
+
+                     if(itr== (*m1).end()){
+                             return NULL;
+                                                }
+                        else {
+                        string temp= to_string(itr->second);
+                        struct node* t1= newNode(temp);
+
+                        st2.push(t1);
+
+                        i=p-1;
+
+                    }
+
+
+                 }
+
+
+
           }
+
 
 
                 while(st1.empty()!=1){
@@ -169,7 +226,7 @@ struct node* createtree(string s){
    int eval(struct node* root){
 
        if (root==NULL)
-        {return -1;}
+        {return INT_MAX;}
 
 
     if (!root->left && !root->right)
@@ -197,7 +254,7 @@ struct node* createtree(string s){
     if (str[0]=='^')
         return pow(l_val,r_val);
 
-    return -1;
+    return  -1;
    }
 
 
@@ -214,19 +271,63 @@ int main()
     for(int i=0;i<n;i++){
       int l,t;
      cin>>t;
+     map<string , int> m2;
   for(int j=0;j<t;j++) {
     string s;
     cin>>s;
-    int l=eval(createtree(s));
-    ans.push_back(l);
+    int flag=0;
+
+    for(int k=0;k<s.length();k++){
+        if(s[k]=='='){
+            flag=1;
+            string subs1,subs2;
+            subs1= s.substr(0,k);
+            subs2= s.substr(k+1,s.length());
+            auto it = m2.find(subs1);
+            if(it== m2.end()){
+                if(eval(createtree(subs2,&m2))==INT_MAX){
+                        ans.push_back(INT_MAX);
+                }
+                else{
+                m2[subs1]=eval(createtree(subs2,&m2));
+            }
+            }
+            else{
+
+                if(eval(createtree(subs2,&m2))==INT_MAX){
+                        ans.push_back(INT_MAX);
+                }
+                else{
+                it->second = eval(createtree(subs2,&m2));
+            }
+}
+        }
+
+
+
+    }
+    if(flag ==0){
+                int l=eval(createtree(s,&m2));
+                ans.push_back(l);
+        }
+
+
+
+
 
 
 }
 
 }
  for(int p=0;p<ans.size();p++){
+     if(ans[p]==INT_MAX){cout<<"CANT BE EVALUATED"<<endl;}
+     else{
    cout<<ans[p]<<endl;
-}
+
+     }
+     }
+
     return 0;
 }
+
 
